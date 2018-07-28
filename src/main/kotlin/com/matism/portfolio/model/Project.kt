f0bcf50f45682ca7860b1a7c94bb7a0f078095bb
Matias San Martin Acosta <msanmartinacosta@gmail.com>
2018-07-28 19:00:09 -0300

    Attribute to val

--- src/main/kotlin/com/matism/portfolio/model/Project.kt
+++ src/main/kotlin/com/matism/portfolio/model/Project.kt
@@ -4,7 +4,7 @@ import java.util.UUID
 
 data class Project(var name: String) {
 
-    var id: String = UUID.randomUUID().toString()
+    val id: String = UUID.randomUUID().toString()
     var description: String = ""
 
 }
